e8aa260a863c189df1b530167428c4b24f664f6d
fletcherist <romanovexports@gmail.com>
2023-10-24 02:19:23 +0300

    feat: add some content

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 import Image from 'next/image'
 
 
+import { BackgroundSlider, Arrow, PhotoGallery, FAQ } from '../components/components'
 
-import { BackgroundSlider, Arrow, PhotoGallery } from '../components/components'
 
 export default function Home() {
 
-  const backgroundImageUrl = 'https://downloader.disk.yandex.ru/preview/7c934c34aeb3afccf224c723f98841ce270e22cb26e7c0a8d9f002c3e5b8a3dd/65293fe3/L1sEtzb6m2fyknzu_fd_YnrYpZEqDsNpW6nAFt2plIsrgbMBIr7R9wmjRk2sv0JgkDL4dWbgZgAB2B9raCXXnQ%3D%3D?uid=0&filename=39f89fbd0aea0779c5a622aa8e221ec9.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=1466x1782'
+  // const backgroundImageUrl = 'https://downloader.disk.yandex.ru/preview/7c934c34aeb3afccf224c723f98841ce270e22cb26e7c0a8d9f002c3e5b8a3dd/65293fe3/L1sEtzb6m2fyknzu_fd_YnrYpZEqDsNpW6nAFt2plIsrgbMBIr7R9wmjRk2sv0JgkDL4dWbgZgAB2B9raCXXnQ%3D%3D?uid=0&filename=39f89fbd0aea0779c5a622aa8e221ec9.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=1466x1782'
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className='w-full h-screen relative'>
+    <main className="flex min-h-screen flex-col items-center justify-between max-w-2xl">
+      <div className='w-full h-[92vh] relative'>
         <BackgroundSlider />
         {/* <div className='absolute inset-0 bg-black opacity-20'></div> */}
-        <div className='absolute inset-0' style={{ backgroundImage: 'linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0))' }}></div>
+        <div className='absolute inset-0' style={{ backgroundImage: 'linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0))' }} />
         <div className='h-full flex flex-col justify-center px-4 md:px-20 relative z-10'>
           <div className='text-gray-200 font-bold text-2xl md:text-4xl lg:text-5xl'>
             Откройте для себя Тибет
@@ -29,13 +29,102 @@ export default function Home() {
         </div>
       </div>
 
-      <ul className="list-disc list-inside">
+
+      <div className='p-4'>
+        {/* <div className='text-center text-gray-900 text-2xl font-bold md:text-4xl lg:text-5xl py-2 tracking-widest'>
+          OPEN TIBET
+        </div> */}
+        <div className='flex justify-center'>
+          <img src="https://storage.googleapis.com/opentibet/logo-1.jpg" width={200} />
+        </div>
+
+        <div className='py-4'>
+          <h2 className="mb-2 text-lg font-semibold text-gray-900">OPEN TIBET - это совместный туристический проект</h2>
+          <ul className="max-w-md space-y-1 text-gray-900 list-disc list-inside ">
+            <li>
+              Тибетского туристического офиса (правительство Тибета)
+            </li>
+            <li>
+              Tash Delek travel agency, Тибет, Лхаса (<a href="https://www.tibettour.travel" className='text-sm font-semibold text-blue-500 hover:text-blue-700' target='_blank'>https://www.tibettour.travel</a>)
+            </li>
+            <li>
+              Kailash-explorer agency, Россия, Санкт Петербург
+            </li>
+          </ul>
+
+          <p className='py-6'>
+            Ключевая цель проекта - сделать Тибет <b>доступнее для всех желающих</b>. Поэтому в рамках данного проекта мы заключили эксклюзивные контракты и смогли добиться наилучших предложений чтобы гарантировать поломникам:.
+            <ul className='py-4 max-w-md space-y-1 list-inside'>
+              <li className="flex items-center">
+                <svg className="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                размещение в хороших отелях
+              </li>
+              <li className="flex items-center">
+                <svg className="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                качественный транспорт
+              </li>
+              <li className="flex items-center">
+                <svg className="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                квалифицированных гидов
+              </li>
+              <li className="flex items-center">
+                <svg className="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                по самым низким ценам
+              </li>
+            </ul>
+          </p>
+        </div>
+      </div>
+
+      {/* <ul className="list-disc list-inside">
         <li className="mb-2">Высокогорные пейзажи: Тибет славится своими величественными высокогорными пейзажами, включая Монт-Эверест и Канченджунгу. Эти места предоставляют непередаваемые виды и приключения для любителей горных походов.</li>
         <li className="mb-2">Духовное наследие: Тибет - родина буддизма и дом для множества духовных практик и ритуалов. Монастыри и храмы, находящиеся среди гор и долин, погружают вас в атмосферу покоя и медитации.</li>
         <li className="mb-2">Культурная уникальность: Культура Тибета удивляет своей уникальностью, включая традиционные праздники, музыку, танцы и удивительные ремесла. Этот регион также известен своей гостеприимностью и теплотой его народа.</li>
         <li className="mb-2">Духовные практики: Тибет предоставляет возможность погрузиться в духовные практики и медитации, обрести внутренний покой и гармонию.</li>
         <li className="mb-2">Природная красота: Высокогорные озера, такие как Намтсо и Манасаровар, являются жемчужинами Тибета. Здесь вы можете наслаждаться кристально чистой водой и окружающей красотой.</li>
-      </ul>
+      </ul> */}
+
+      <div >
+        <div className='flex px-4'>
+          <div className="w-32 h-32">
+            <img
+              className="object-cover rounded-full w-full h-full"
+              src="https://storage.googleapis.com/opentibet/alexey-portrait.jpeg"
+              alt="profile picture"
+            />
+          </div>
+          <div className='flex items-center flex-1 pl-4'>
+            <div>
+              <div className='text-xl font-bold text-gray-800'>Алексей Перчуков</div>
+              <div className='text-sm text-gray-400 pt-1'>Координатор проекта</div>
+              <a href='https://www.instagram.com/kailash_explorer/' className='text-sm font-semibold text-blue-500 hover:text-blue-700' target='_blank'>@kailash_explorer</a>
+            </div>
+          </div>
+        </div>
+        <div className='p-4'>
+          Автор нескольких профессиональных путеводителей по Тибету на русском языке, со-автор фильма “Тибет, место которое меняет жизнь”. Докладчик многочисленных международных конференции, посвященных Кайласу. Автор статьи “ Спиральная Кора”, исследователь , который поднялся на все плечи Кайласа и посетил все священные долины Мандалы Кайласа.
+        </div>
+        <div className="grid grid-cols-2 gap-1">
+          <img src="https://storage.googleapis.com/opentibet/alex-1.jpg" className="col-start-1 col-end-3 rounded-sm cursor-pointer" />
+          <img src="https://storage.googleapis.com/opentibet/alex-6.jpg" className="col-start-1 col-end-2 row-start-2 row-end-4 rounded-sm cursor-pointer" />
+          <img src="https://storage.googleapis.com/opentibet/alex-2.jpg" className="col-start-2 col-end-3 row-start-2 row-end-3 rounded-sm cursor-pointer" />
+          <img src="https://storage.googleapis.com/opentibet/alex-4.jpg" className="col-start-2 col-end-3 rounded-sm cursor-pointer" />
+        </div>
+
+        <div className='text-center p-4'>
+          <p className='text-gray-400 text-sm'>
+            Больше фото в <a href='https://www.instagram.com/kailash_explorer/' className='text-blue-500 hover:text-blue-700' target='_blank'>Instagram</a>
+          </p>
+        </div>
+      </div>
 
       <div className='w-full'>
         <div className='px-4 py-2'>
@@ -46,6 +135,10 @@ export default function Home() {
             снятый{' '}
             <a href='https://www.instagram.com/kailash_explorer/' className='text-blue-500 hover:text-blue-700' target='_blank'>Алексеем Перчуковым</a>
           </div>
+
+          <p className='py-4 pb-0'>
+            Мы организуем регулярные ежемесячные туры в Тибет по уникальной программе. Пример программы можно посмотреть в фильме “Тибет, место которое меняет жизнь”, снятом в одном из таких туров:
+          </p>
         </div>
         <div className='w-full'>
           <iframe
@@ -59,9 +152,6 @@ export default function Home() {
         </div>
       </div>
 
-      <img className="rounded-full w-24 h-24" src="https://downloader.disk.yandex.ru/preview/6f2b4409163903f3bae6b144ff90a3dcb42c8c89203c2fad0de268ed033a4e18/652975df/xWUaUHU8xdhCmr-NJ2FpAz00V0ukOKc1QLF-u_GKOIQZzEouq2ftScfrtTJi3Q-eSMC2yxyfSOrHv-rXslD2Kg%3D%3D?uid=0&filename=a36b8b0447091e9dc61a60b2c95f72f9.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=1098x1227" alt="profile picture" />
-
-      <PhotoGallery />
       <div>
         <div className='text-gray-900 font-semibold text-2xl md:text-4xl lg:text-5xl py-10 text-center'>
           Почему стоит ехать<br /> в Тибет с нами?
@@ -113,6 +203,27 @@ export default function Home() {
         </figure>
       </div>
 
+      <div className='text-gray-900 font-semibold text-2xl md:text-4xl lg:text-5xl py-2'>
+        Часто задаваемые вопросы
+      </div>
+      <FAQ />
+
+      <div className='text-gray-900 font-semibold text-2xl md:text-4xl lg:text-5xl py-2'>
+        Фото с прошлых туров
+      </div>
+      <PhotoGallery />
+
+      <div className='relative'>
+        <div className='absolute inset-0' style={{ backgroundImage: 'linear-gradient(to top, rgba(0,0,0,1), rgba(0,0,0,0))' }} />
+        <div className='absolute h-full w-full'>
+          <div className='flex h-full items-center justify-center'>
+            <div className='text-gray-200 font-bold text-3xl md:text-4xl lg:text-5xl p-8'>
+              До втречи в Тибете.
+            </div>
+          </div>
+        </div>
+        <video src='https://storage.googleapis.com/opentibet/doors.mp4' autoPlay muted loop></video>
+      </div>
       <Copyright />
     </main>
   )
@@ -135,5 +246,6 @@ const Copyright: React.FC = () => {
     </div>
   );
 };
+
 
 
